a488546e7f36b2e5fdfee86f0842416d35ac1b72
tuanbd5 <tuanbd5@fpt.com>
2023-12-22 16:26:55 +0900

    ichiou

--- src/components/ColletionSlider.js
+++ src/components/ColletionSlider.js
@@ -44,21 +44,23 @@ const ColletionSlider = (props) => {
     const sliderItem = images.map((item, index) => {
         return (
             <div className="red text-start" key={index}>
-                <Card className="mx-1"   >
-                    <div style={collectionStyle}></div>
-                    <CardBody>
-                        <div className="red " style={{ display: "flex", justifyContent: "space-between" }}>
-                            <span className="text-danger fw-bold">$cost</span>
-                            <span>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-bookmark-heart" viewBox="0 0 16 16">
-                                    <path fillRule="evenodd" d="M8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z" />
-                                    <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z" /></svg>
-                            </span>
-                        </div>
-                        <div className="red h5 fw-semibold">name</div>
-                        <div className="red">color</div>
-                    </CardBody>
-                </Card>
+                <Link to={"/detail-item"} style={{ textDecoration: "none" }}>
+                    <Card className="mx-1"   >
+                        <div style={collectionStyle}></div>
+                        <CardBody>
+                            <div className="red " style={{ display: "flex", justifyContent: "space-between" }}>
+                                <span className="text-danger fw-bold">$cost</span>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-bookmark-heart" viewBox="0 0 16 16">
+                                        <path fillRule="evenodd" d="M8 4.41c1.387-1.425 4.854 1.07 0 4.277C3.146 5.48 6.613 2.986 8 4.412z" />
+                                        <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z" /></svg>
+                                </span>
+                            </div>
+                            <div className="red h5 fw-semibold">name</div>
+                            <div className="red">color</div>
+                        </CardBody>
+                    </Card>
+                </Link>
             </div>
 
         )
