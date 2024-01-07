902faaf99f91ae7a90efa25390fd57aaf5477107
tunbui <tuanbui2122@gmail.com>
2024-01-08 00:17:15 +0900

    second

--- src/components/Payment/Payment.js
+++ src/components/Payment/Payment.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 const Payment = () => {
     const [isShiptobuyer, setIsShiptobuyer] = useState(true);
+    const [isShipping, setIsShipping] = useState(true);
+    const [isPaymentMethod, setIsPaymentMethod] = useState(false);
 
 
     const handleCheckboxChange = () => {
@@ -12,15 +14,18 @@ const Payment = () => {
 
             <div className="container red ">
                 <div className="row p-3 red " >
+
+
                     <div className="col-7 px-6 text-center red ">
+
                         <div className="logo red"><h1>LOGO</h1></div>
                         <div className=" mb-2 red" > cần 3 bước với icon cụ thể </div>
                         <div className=" mb-2 red text-start fw-bold" ><h4>  thông tin khách hàng</h4> </div>
 
-                        <input className="form-control mb-2 red" name="email" type="text" placeholder="Default input" aria-label="default input example" />
-                        <input className="form-control mb-2 red" name="Name" type="text" placeholder="Default input" aria-label="default input example" />
-                        <input className="form-control mb-2 red" type="text" placeholder="Default input" aria-label="default input example" />
-                        <input className="form-control mb-2 red" type="text" placeholder="Default input" aria-label="default input example" />
+                        <input className="form-control mb-2 red" type="text" placeholder="email" aria-label="default input example" />
+                        <input className="form-control mb-2 red" type="text" placeholder="tên" aria-label="default input example" />
+                        <input className="form-control mb-2 red" type="text" placeholder="sdt" aria-label="default input example" />
+                        <input className="form-control mb-2 red" type="text" placeholder="địa chỉ" aria-label="default input example" />
 
                         <div className="row  red d-flex justify-content-center mb-2" >
 
@@ -51,10 +56,10 @@ const Payment = () => {
                             <div className="mt-3">
 
                                 <div className=" mb-2 red text-start fw-bold" ><h4>  thông tin người nhận</h4> </div>
-                                <input className="form-control mb-2 red" type="text" placeholder="Default input" aria-label="default input example" />
-                                <input className="form-control mb-2 red" type="text" placeholder="Default input" aria-label="default input example" />
-                                <input className="form-control mb-2 red" type="text" placeholder="Default input" aria-label="default input example" />
-                                <input className="form-control mb-2 red" type="text" placeholder="Default input" aria-label="default input example" />
+                                <input className="form-control mb-2 red" type="text" placeholder="email" aria-label="default input example" />
+                                <input className="form-control mb-2 red" type="text" placeholder="tên" aria-label="default input example" />
+                                <input className="form-control mb-2 red" type="text" placeholder="sdt" aria-label="default input example" />
+                                <input className="form-control mb-2 red" type="text" placeholder="địa chỉ" aria-label="default input example" />
 
                                 <div className="row  red d-flex justify-content-center mb-2" >
 
@@ -69,14 +74,13 @@ const Payment = () => {
                                     </div>
 
                                 </div>
-                                <div className="text-start mb-2 red">
 
-                                    <input type="checkbox" /> Giao hàng đến địa chỉ khác
-                                </div>
 
 
                             </div>
                         }
+
+
                         <div className="input-group mb-2 red">
 
                             <textarea className="form-control red" aria-label="With textarea"></textarea>
@@ -87,27 +91,70 @@ const Payment = () => {
 
                         </div>
 
-                        {/* từ đây là trang sau */}
+                        {isShipping === true &&
 
-                        <div>
-                            <hr />
-                            <div className="border border-dark text-start">
-                                <div> thông tin nhận hàng</div>
-                                <div> tên </div>
-                                <div> địa chỉ </div>
-                                <div> ghi chú nếu có </div>
+                            <div>
+                                <hr />
+                                <div className="border border-secondary text-start ps-3">
+                                    <div> thông tin nhận hàng</div>
+                                    <div> tên </div>
+                                    <div> email </div>
+                                    <div> Sdt </div>
+                                    <div> địa chỉ </div>
+                                    <div> ghi chú nếu có </div>
+                                </div>
+
+
+                                <div className="  form-control form-check text-start border border-secondary mt-3" targetid="flexRadioDefault1">
+                                    <input className="form-check-input red  " style={{ marginLeft: "-13px" }} type="radio" name="flexRadioDefault" id="flexRadioDefault1" defaultChecked />
+                                    <label className="form-check-label ms-3" htmlFor="flexRadioDefault1">
+                                        phương thức vận chuyển 1
+                                    </label>
+                                </div>
+                                <div className="  form-control form-check text-start border border-secondary mt-3">
+                                    <input className="form-check-input red  " style={{ marginLeft: "-13px" }} type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
+                                    <label className="form-check-label ms-3" htmlFor="flexRadioDefault2">
+                                        phương thức vận chuyển 1
+                                    </label>
+                                </div>
+                                <div className="  form-control form-check text-start border border-secondary mt-3">
+                                    <input className="form-check-input red  " style={{ marginLeft: "-13px" }} type="radio" name="flexRadioDefault" id="flexRadioDefault3" />
+                                    <label className="form-check-label ms-3" htmlFor="flexRadioDefault3">
+                                        phương thức thanh toán 1
+                                    </label>
+                                </div>
+                                <div className="  form-control form-check text-start border border-secondary mt-3">
+                                    <input className="form-check-input red  " style={{ marginLeft: "-13px" }} type="radio" name="flexRadioDefault" id="flexRadioDefault4" />
+                                    <label className="form-check-label ms-3" htmlFor="flexRadioDefault4">
+                                        phương thức thanh toán 2
+                                    </label>
+                                </div>
+
+
+
+
+
+                                <div className="row d-flex justify-content-between mb-2 red">
+                                    <button className="col-1 btn btn-primary red">return</button>
+                                    <button className="col-5 btn btn-primary red">next</button>
+
+                                </div>
+
+                                {/* chọn phương thức thanh toán */}
+                                <hr />
+                                <div>
+
+
+
+
+
+                                </div>
+                                {/* cho tới đây nè */}
+
                             </div>
-                            <div> chọn phương thức giao hàng 1</div>
-                            <div> chọn phương thức giao hàng 1</div>
-                            <div className="row d-flex justify-content-between mb-2 red">
-                                <button className="col-1 btn btn-primary red">return</button>
-                                <button className="col-5 btn btn-primary red">next</button>
 
-                            </div>
+                        }
 
-                        </div>
-
-                        {/* tới đây */}
 
                     </div>
                     <div className="col-5 red">
